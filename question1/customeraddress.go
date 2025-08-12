76479bbbfaf1cf25bf855280173ca56967cf30b1
cmcaughren <cmcaughren@gmail.com>
2025-08-11 21:58:29 -0700

    Added add customerAddress endpoint, question1

--- question1/customeraddress.go
+++ question1/customeraddress.go
@@ -79,3 +79,89 @@ func GetCustomerAddress(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(customerAddress)
 }
+
+// Add a new customer address
+func AddCustomerAddress(w http.ResponseWriter, r *http.Request) {
+	var ca CustomerAddress
+	json.NewDecoder(r.Body).Decode(&ca)
+	valid, errMsg := validateCustomerAddress(ca)
+	if !valid {
+		fmt.Print("Bad customer data: &v\n", errMsg)
+		http.Error(w, errMsg, http.StatusBadRequest)
+		return
+	}
+
+	//"OUTPUT" with QueryRow will get us a return value - use of Exec to insert would give none
+	query := `INSERT INTO CustomerAddress (CustomerID, Line1, Line2, City, StateProvince, Country) 
+	OUTPUT INSERTED.CustomerID, INSERTED.Line1, INSERTED.Line2, INSERTED.City, INSERTED.Stateprovince, INSERTED.Country
+	VALUES (@p1, @p2, @p3, @p4, @p5, @p6)`
+	var newCustomerAddress CustomerAddress
+
+	err := db.QueryRow(query,
+		ca.CustomerID,
+		ca.Line1,
+		ca.Line2,
+		ca.City,
+		ca.StateProvince,
+		ca.Country).Scan(
+		&newCustomerAddress.CustomerID,
+		&newCustomerAddress.Line1,
+		&newCustomerAddress.Line2,
+		&newCustomerAddress.City,
+		&newCustomerAddress.StateProvince,
+		&newCustomerAddress.Country)
+	if err != nil {
+		fmt.Printf("Error adding customer address: %v\n", err)
+		http.Error(w, "Error adding customer address", http.StatusInternalServerError)
+		return
+	}
+	//Send JSON response
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newCustomerAddress)
+}
+
+func validateCustomerAddress(customerAddress CustomerAddress) (bool, string) {
+	// Validate CustomerID
+	if customerAddress.CustomerID <= 0 {
+		return false, "CustomerID is required and must be positive"
+	}
+
+	// Validate Line1
+	if len(customerAddress.Line1) == 0 {
+		return false, "Line1 is required"
+	}
+	if len(customerAddress.Line1) > 100 {
+		return false, "Line1 must be 100 characters or less"
+	}
+
+	// Validate Line2 (optional but has max length)
+	if len(customerAddress.Line2) > 100 {
+		return false, "Line2 must be 100 characters or less"
+	}
+
+	// Validate City
+	if len(customerAddress.City) == 0 {
+		return false, "City is required"
+	}
+	if len(customerAddress.City) > 50 {
+		return false, "City must be 50 characters or less"
+	}
+
+	// Validate StateProvince
+	if len(customerAddress.StateProvince) == 0 {
+		return false, "StateProvince is required"
+	}
+	if len(customerAddress.StateProvince) > 50 {
+		return false, "StateProvince must be 50 characters or less"
+	}
+
+	// Validate Country
+	if len(customerAddress.Country) == 0 {
+		return false, "Country is required"
+	}
+	if len(customerAddress.Country) > 50 {
+		return false, "Country must be 50 characters or less"
+	}
+
+	return true, ""
+}
