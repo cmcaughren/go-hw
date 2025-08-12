c0616a5de2a4da1fe394a7fb9134f298a007f89b
cmcaughren <cmcaughren@gmail.com>
2025-08-11 21:36:01 -0700

    Added get customerAddress endpoint, question1

--- question1/customeraddress.go
+++ question1/customeraddress.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	_ "github.com/microsoft/go-mssqldb"
 )
 
@@ -47,4 +49,33 @@ func GetCustomerAddresses(w http.ResponseWriter, r *http.Request) {
 	//Send JSON reponse
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(customerAddresses)
+}
+
+// Get a single customer address, by CustomerAddressID
+func GetCustomerAddress(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["CustomerAddressID"]
+
+	query := "SELECT * FROM CustomerAddress WHERE CustomerAddressID = @p1"
+	var customerAddress CustomerAddress
+	err := db.QueryRow(query, id).Scan(&customerAddress.CustomerAddressID,
+		&customerAddress.CustomerID,
+		&customerAddress.Line1,
+		&customerAddress.Line2,
+		&customerAddress.City,
+		&customerAddress.StateProvince,
+		&customerAddress.Country)
+	if err == sql.ErrNoRows {
+		fmt.Printf("CustomerAddress Not Found: %v\n", err)
+		http.Error(w, "CustomerAddress Not Found", http.StatusNotFound)
+		return
+	} else if err != nil {
+		fmt.Printf("Scan error: %v\n", err)
+		http.Error(w, "Failed to fetch customer", http.StatusInternalServerError)
+		return
+	}
+
+	//Send JSON response
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(customerAddress)
 }
